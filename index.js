e04cff9c4d0f5bab0b937393ce64182e1f5a3ff6
RQH <656418510@qq.com>
2020-01-15 07:51:41 +0800

    add div container for images

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ if (hexo.config.lazyload.post && hexo.config.lazyload.post.only) {
     hexo.extend.filter.register('after_post_render', require('./lib/process').processPost);
 }
 else {
-    // hexo.extend.filter.register('after_render:html', require('./lib/process').processSite);
-    hexo.extend.filter.register('after_render:html', require('./lib/process-container').processSite);
+    hexo.extend.filter.register('after_render:html', require('./lib/process').processSite);
 }
 
 hexo.extend.filter.register('after_render:html', require('./lib/addscripts').addScript);
